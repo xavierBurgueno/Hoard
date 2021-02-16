da5aace5a3f271c386c2002b644bf86816149bc0
Xavier Burgueno <xavierburgueno.xb@gmail.com>
2021-02-15 22:45:15 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hoards
+
+Developed with Unreal Engine 4
---
+++ Source/Hoards/HoardsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HoardsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOARDS_API AHoardsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
