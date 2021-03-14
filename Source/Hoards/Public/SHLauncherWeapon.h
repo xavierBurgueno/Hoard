3cf18025e3823825ae1f3b27a710a19d67ad0e70
Xavier Burgueno <xavierburgueno.xb@gmail.com>
2021-03-14 00:35:14 -0800

    Added Grenade Lancher and Projectile

---
+++ Source/Hoards/Public/SHLauncherWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SHWeapon.h"
+#include "SHLauncherWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOARDS_API ASHLauncherWeapon : public ASHWeapon
+{
+	GENERATED_BODY()
+	
+};
